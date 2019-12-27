ae7e66270723dc576dcb8f20e866648f82ad33bf
maxiaoqu <maxiaoqu@163.com>
2019-12-27 17:09:42 +0800

    head

--- example/.vuepress/config.js
+++ example/.vuepress/config.js
@@ -1,23 +1,11 @@
 const themeConfig = require('./config/theme/')
 const plugins = require('./config/plugins')
+const head = require('./config/head')
 
 module.exports = {
   dest: 'public',
   base: '/ido-design/',
-  head: [
-    ['link', {rel: 'icon', href: '/favicon.ico'}],
-    ['meta', {name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no'}],
-    ['meta', {name: 'author', content: 'maxiaoqu'}],
-    ['meta', {name: 'keywords', content: 'vuepress,ido,maxiaoqu,vuepress-ido,vuepress-theme-ido,theme,blog,主题'}],
-    ['link', {rel: 'manifest', href: '/manifest.json'}],
-    ['meta', {name: 'theme-color', content: '#42b983'}],
-    ['meta', {name: 'apple-mobile-web-app-capable', content: 'yes'}],
-    ['meta', {name: 'apple-mobile-web-app-status-bar-style', content: 'black'}],
-    ['link', {rel: 'apple-touch-icon', href: '/icon_vuepress_ido.png'}],
-    ['link', {rel: 'mask-icon', href: '/icon_vuepress_ido.svg', color: '#42b983'}],
-    ['meta', {name: 'msapplication-TileImage', content: '/icon_vuepress_ido.png'}],
-    ['meta', {name: 'msapplication-TileColor', content: '#000000'}],
-  ],
+  head: head,
   theme: require.resolve('../../vuepress-theme-ido'),
   themeConfig,
   locales: {
--- example/.vuepress/config/head/index.ts
+++ example/.vuepress/config/head/index.ts
@@ -1,37 +1,14 @@
-const path = require('path')
 module.exports = [
-  [
-    '@vuepress/pwa',
-    {
-      serviceWorker: true,
-      updatePopup: {
-        message: "发现新内容可用",
-        buttonText: "刷新"
-      }
-    }
-  ],
-  [
-    '@vuepress/google-analytics',
-    {
-      ga: 'UA-149716079-2'
-    }
-  ],
-  [
-    '@vuepress/plugin-register-components',
-    {
-      components: [
-        {
-          name: 'ido-home-page-one',
-          path: path.resolve(__dirname, './components/HomePageOne.vue')
-        }
-      ],
-      componentsDir: path.resolve(__dirname, './demo')
-    }
-  ],
-  '@ido-design/extract-code',
-  'flowchart',
-  ['sitemap', {
-    hostname: 'https://ido-design.maxiaoqu.com/vuepress-theme-ido'
-  }],
-  require('./plugins/notification/index')
+  ['link', {rel: 'icon', href: '/favicon.ico'}],
+  ['meta', {name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no'}],
+  ['meta', {name: 'author', content: 'maxiaoqu'}],
+  ['meta', {name: 'keywords', content: 'vuepress,ido,maxiaoqu,vuepress-ido,vuepress-theme-ido,theme,blog,主题'}],
+  ['link', {rel: 'manifest', href: '/manifest.json'}],
+  ['meta', {name: 'theme-color', content: '#42b983'}],
+  ['meta', {name: 'apple-mobile-web-app-capable', content: 'yes'}],
+  ['meta', {name: 'apple-mobile-web-app-status-bar-style', content: 'black'}],
+  ['link', {rel: 'apple-touch-icon', href: '/icon_vuepress_ido.png'}],
+  ['link', {rel: 'mask-icon', href: '/icon_vuepress_ido.svg', color: '#42b983'}],
+  ['meta', {name: 'msapplication-TileImage', content: '/icon_vuepress_ido.png'}],
+  ['meta', {name: 'msapplication-TileColor', content: '#000000'}],
 ]
