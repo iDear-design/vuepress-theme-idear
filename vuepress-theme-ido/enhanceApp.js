2b7b6a5286a88531f82f4477965319cd789f4060
maxiaoqu <maxiaoqu@163.com>
2020-01-08 20:00:32 +0800

    修复字体图标的问题

--- vuepress-theme-ido/enhanceApp.js
+++ vuepress-theme-ido/enhanceApp.js
@@ -11,7 +11,7 @@ export default ({
   Vue.mixin(postMixin)
   Vue.mixin(localMixin)
   if (!isServer) {
-    addLinkToHead('//at.alicdn.com/t/font_1030519_2ciwdtb4x65.css')
+    addLinkToHead('//files.maxiaoqu.com/iconfont/font_vuepress_theme_ido/iconfont.css')
     registerCodeThemeCss(siteData.themeConfig.codeTheme)
   }
 }
