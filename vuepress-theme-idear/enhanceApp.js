72d6b0e14016905a28616b9208243e239a46f123
maxiaoqu <maxiaoqu@163.com>
2021-02-26 12:42:46 +0800

    配置文件

---
+++ vuepress-theme-idear/enhanceApp.js
@@ -0,0 +1,17 @@
+import postMixin from '@theme/mixins/posts'
+import localMixin from '@theme/mixins/locales'
+import { addLinkToHead } from '@theme/helpers/utils'
+import { registerCodeThemeCss } from '@theme/helpers/other'
+
+export default ({
+  Vue,
+  siteData,
+  isServer
+}) => {
+  Vue.mixin(postMixin)
+  Vue.mixin(localMixin)
+  if (!isServer) {
+    addLinkToHead('//files.maxiaoqu.com/iconfont/font_vuepress_theme_timi/iconfont.css')
+    registerCodeThemeCss(siteData.themeConfig.codeTheme)
+  }
+}
