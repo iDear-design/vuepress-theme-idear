54e17358d3f1abfd91c8dc3b1b33ea812916cf8b
maxiaoqu <maxiaoqu@163.com>
2021-03-06 12:29:06 +0800

    plugin

--- example/.vuepress/plugins/notification/index.js
+++ example/.vuepress/plugins/notification/index.js
@@ -1,7 +1,7 @@
 const { path } = require('@vuepress/shared-utils')
 
 module.exports = (options, context) => ({
-  name: '@idear-vuepress/notification',
+  name: '@idear-vuepress/vuepress-plugin-notification',
   enhanceAppFiles: [
     path.resolve(__dirname, './bin/enhanceAppFile.js')
   ],
