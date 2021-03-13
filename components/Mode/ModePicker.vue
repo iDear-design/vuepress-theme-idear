0ce825795ca66abb967fc429d9e5fcc965fd6293
maxiaoqu <maxiaoqu@163.com>
2021-03-13 10:22:18 +0800

    mode

--- components/Mode/ModePicker.vue
+++ components/Mode/ModePicker.vue
@@ -7,30 +7,28 @@
         :key="index"
         :class="getClass(mode.mode)"
         @click="selectMode(mode.mode)"
-      >{{ mode.title }}
-      </li>
+      >{{ mode.title }}</li>
     </ul>
   </div>
 </template>
 
 <script>
 import applyMode from './applyMode'
-
 export default {
   name: 'ModeOptions',
 
-  data() {
+  data () {
     return {
       modeOptions: [
-        {mode: 'dark', title: 'dark'},
-        {mode: 'auto', title: 'auto'},
-        {mode: 'light', title: 'light'}
+        { mode: 'dark', title: 'dark' },
+        { mode: 'auto', title: 'auto' },
+        { mode: 'light', title: 'light' }
       ],
       currentMode: 'auto'
     }
   },
 
-  mounted() {
+  mounted () {
     // modePicker 开启时默认使用用户主动设置的模式
     this.currentMode = localStorage.getItem('mode') || this.$themeConfig.mode || 'auto'
 
@@ -48,14 +46,14 @@ export default {
   },
 
   methods: {
-    selectMode(mode) {
+    selectMode (mode) {
       if (mode !== this.currentMode) {
         this.currentMode = mode
         applyMode(mode)
         localStorage.setItem('mode', mode)
       }
     },
-    getClass(mode) {
+    getClass (mode) {
       return mode !== this.currentMode ? mode : `${mode} active`
     }
   }
@@ -63,8 +61,6 @@ export default {
 </script>
 
 <style lang="stylus">
-@require '../../styles/mode.styl'
-
 .mode-options
   background-color var(--background-color)
   min-width: 125px;
@@ -72,18 +68,16 @@ export default {
   padding: 1em;
   box-shadow var(--box-shadow);
   border-radius: $borderRadius;
-
   .title
     margin-top 0
     margin-bottom .6rem
     font-weight bold
     color var(--text-color)
-
   .color-mode-options
     display: flex;
     flex-wrap wrap
-
     li
+      flex: 1;
       text-align: center;
       font-size 12px
       color var(--text-color)
@@ -93,19 +87,15 @@ export default {
       border-bottom 1px solid #666
       background-color var(--background-color)
       cursor pointer
-
       &.dark
         border-radius: $borderRadius 0 0 $borderRadius
         border-left 1px solid #666
-
       &.light
         border-radius: 0 $borderRadius $borderRadius 0
         border-right 1px solid #666
-
       &.active
         background-color: $accentColor;
         color #fff
-
       &:not(.active)
         border-right 1px solid #666
 </style>
--- components/Mode/index.vue
+++ components/Mode/index.vue
@@ -1,17 +1,18 @@
 <template>
-  <div v-click-outside="hideMenu" class="color-picker" v-if="$themeConfig.modePicker !== false">
-    <a class="color-button" @click.prevent="showMenu = !showMenu">
-      <i class="iconfont idear-color"></i>
-    </a>
-    <transition name="menu-transition" mode="out-in">
-      <div v-show="showMenu" class="color-picker-menu">
-        <ModePicker/>
-      </div>
-    </transition>
-  </div>
+	<div v-click-outside="hideMenu" class="color-picker" v-if="$themeConfig.modePicker !== false" >
+		<a class="color-button" @click.prevent="showMenu = !showMenu">
+      <idear-icon icon="idear-color" />
+		</a>
+		<ModuleTransition :transform=" ['translate(-50%, 0)', 'translate(-50%, -10px)']">
+			<div v-show="showMenu" class="color-picker-menu">
+				<ModePicker />
+			</div>
+		</ModuleTransition>
+	</div>
 </template>
 
 <script>
+import { IdearIcon, ModuleTransition } from '@theme/components/IdearCore'
 import ClickOutside from 'vue-click-outside'
 import ModePicker from './ModePicker'
 import applyMode from './applyMode'
@@ -24,20 +25,22 @@ export default {
   },
 
   components: {
-    ModePicker
+    ModePicker,
+    IdearIcon,
+    ModuleTransition
   },
 
-  data() {
+  data () {
     return {
       showMenu: false
     }
   },
 
   // 为了在保证 modePicker 在 SSR 中正确开关，并实现管理，Mode 组件将负责 modePicker 关闭的情况
-  mounted() {
+  mounted () {
     // modePicker 关闭时默认使用主题设置的模式
     const themeMode = this.$themeConfig.mode || 'auto'
-    const {modePicker} = this.$themeConfig
+    const { modePicker } = this.$themeConfig
     if (modePicker === false) {
       // 为 'auto' 模式设置监听器
       if (themeMode === 'auto') {
@@ -53,7 +56,7 @@ export default {
   },
 
   methods: {
-    hideMenu() {
+    hideMenu () {
       this.showMenu = false
     }
   }
@@ -61,60 +64,42 @@ export default {
 </script>
 
 <style lang="stylus">
-@require '../../styles/mode.styl'
-
 .color-picker {
-  position: relative;
-  margin-right: 1em;
+	position: relative;
+	margin-right: 1em;
   cursor pointer;
+	.color-button {
+		align-items: center;
+		height: 100%;
+		.iconfont {
+			font-size 1.4rem
+			color: $accentColor
+		}
+	}
 
-  .color-button {
-    align-items: center;
-    height: 100%;
+	.color-picker-menu {
+		position: absolute;
+		top: 40px;
+		left: 50%;
+		z-index: 150;
 
-    .iconfont {
-      font-size 1.4rem
-      color: $accentColor
-    }
-  }
-
-  .color-picker-menu {
-    position: absolute;
-    top: 40px;
-    left: 50%;
-    transform: translateX(-50%);
-    z-index: 150;
-
-    &.menu-transition-enter-active,
-    &.menu-transition-leave-active {
-      transition: all 0.25s ease-in-out;
-    }
-
-    &.menu-transition-enter,
-    &.menu-transition-leave-to {
-      top: 50px;
-      opacity: 0;
-    }
-
-    ul {
-      list-style-type: none;
-      margin: 0;
-      padding: 0;
-    }
-  }
+		ul {
+			list-style-type: none;
+			margin: 0;
+			padding: 0;
+		}
+	}
 }
 
 @media (max-width: $MQMobile) {
-  .color-picker {
-    margin-right: 1rem;
-
-    .color-picker-menu {
-      left: calc(50% - 35px);
-
-      &::before {
-        left: calc(50% + 35px);
-      }
-    }
-  }
+	.color-picker {
+		margin-right: 1rem;
+		.color-picker-menu {
+			left: calc(50% - 35px);
+			&::before {
+				left: calc(50% + 35px);
+			}
+		}
+	}
 }
 </style>
